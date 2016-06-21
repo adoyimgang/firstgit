5a4ab66ae56273b886b14b4e79509f06da906aed
ado <adowambo@yahoo.com>
2016-06-21 12:23:54 +0100

    initial version

---
+++ Classement.java
@@ -0,0 +1,33 @@
+public class Classement{
+
+	public static void main(String[] arg){
+
+		int tab[] = {60, 1, 24, 3, 45, 50, 8};
+		int i = 0;
+	//	int j = 0;
+		int x;
+		 
+	//	while(i<tab.length){
+		
+			for(i=0; i<tab.length; i++){
+                         //  System.out.println(i);	
+
+				for(int j=i+1; j<tab.length; j++){
+                                          	
+					if(tab[i]>tab[j]){	
+						x = tab[i];
+						tab[i] = tab[j];
+						tab[j] = x ;
+					}
+				}
+				
+			}
+
+		for(i=0; i<tab.length; i++){
+
+			System.out.print(tab[i]+" ");
+		}
+		
+	//	}
+	}	
+}
